7183f556798fac3d373c0e4a6fe9b5f1de574118
Gildas Quéméner <gildas.quemener@gmail.com>
2023-11-03 10:02:53 +0100

    stay dead

--- tests/GameOfLifeTest.php
+++ tests/GameOfLifeTest.php
@@ -4,13 +4,15 @@ namespace GQ\Kata\Tests;
 
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use SebastianBergmann\Type\VoidType;
 
 final class GameOfLifeTest extends TestCase
 {
 
   private function nextState(Cell $cell, int $aliveNeighboursCount): Cell
   {
+    if ($cell === Cell::Dead && $aliveNeighboursCount === 2) {
+      return Cell::Dead;
+    }
     if ($aliveNeighboursCount === 2 || $aliveNeighboursCount === 3) {
       return Cell::Alive;
     }
@@ -39,6 +41,24 @@ final class GameOfLifeTest extends TestCase
     $cell = Cell::Alive;
     foreach (range(4, 8) as $aliveNeighboursCount) {
       self::assertEquals(Cell::Dead, $this->nextState($cell, $aliveNeighboursCount));
+    }
+  }
+
+  #[Test]
+  public function a_cell_reborns_if_it_has_3_live_neighbours(): void
+  {
+    $cell = Cell::Dead;
+    self::assertEquals(Cell::Alive, $this->nextState($cell, 3));
+  }
+
+  #[Test]
+  public function a_dead_cell_stays_dead_if_does_not_have_3_live_neighbours():void
+  {
+    $cell = Cell::Dead;
+    foreach (range(0, 8) as $aliveNeighboursCount) {
+      if ($aliveNeighboursCount !== 3) {
+        self::assertEquals(Cell::Dead, $this->nextState($cell, $aliveNeighboursCount));
+      }
     }
   }
 }
