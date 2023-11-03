288b91d0bd22b6a11fe78ed9624aa694d73c3255
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2023-11-03 09:46:52 +0100

    adding survival rule

---
+++ tests/GameOfLifeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GQ\Kata\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Type\VoidType;
+
+final class GameOfLifeTest extends TestCase
+{
+
+  private function nextState(Cell $cell, int $aliveNeighboursCount): Cell
+  {
+    if ($aliveNeighboursCount > 1) {
+      return Cell::Alive;
+    }
+    return Cell::Dead;
+  }
+
+  #[Test]
+  public function a_cell_dies_if_less_than_2_live_neighbours(): void
+  {
+    $cell = Cell::Alive;
+    self::assertEquals(Cell::Dead, $this->nextState($cell, 0));
+    self::assertEquals(Cell::Dead, $this->nextState($cell, 1));
+  }
+
+  #[Test]
+  public function a_cell_stays_alive_if_2_or_3_live_neighbours(): void
+  {
+    $cell = Cell::Alive;
+    self::assertEquals(Cell::Alive, $this->nextState($cell, 2));
+    self::assertEquals(Cell::Alive, $this->nextState($cell, 3));
+  }
+}
+
